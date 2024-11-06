1e7158f93138973037f5b98c2fb79066e0af7910
Venkata Sai Charan Bhupathi <venkatasaicharanbhupathi.com>
2024-11-06 17:36:11 +0530

    Update Lead Owner to current user on button click without apex

--- force-app/main/default/lwc/selfAssignLeadWithoutApex/selfAssignLeadWithoutApex.js
+++ force-app/main/default/lwc/selfAssignLeadWithoutApex/selfAssignLeadWithoutApex.js
@@ -1,38 +1,37 @@
 import { api, LightningElement } from 'lwc';
 import USER_ID from '@salesforce/user/Id';
-import OWNER_ID from '@salesforce/schema/Lead.OwnerId';
+import OWNER_FIELD from '@salesforce/schema/Lead.OwnerId';
 import { updateRecord } from 'lightning/uiRecordApi';
-import {RefreshEvent} from 'lightning/refresh';
-import {ShowToastEvent} from 'lightning/platformShowToastEvent';
-
+import { RefreshEvent } from 'lightning/refresh';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 export default class SelfAssignLeadWithoutApex extends LightningElement {
 
     @api recordId;
 
     @api invoke(){
-
         updateRecord({
             fields : {
                 Id : this.recordId,
-                [OWNER_ID.fieldApiName] : USER_ID
+                [OWNER_FIELD.fieldApiName] : USER_ID
             }
         })
         .then((result)=>{
-            this.showToast('SUCCESS','Lead Owner Updated','success');
+            this.showToast('SUCCESS','Updated the Lead Owner','success');
             this.dispatchEvent(new RefreshEvent());
         })
         .catch(error=>{
-            this.showToast('FAILED','Failed to Update the Lead Owner','error');
+            this.showToast('ERROR','Failed to Updated Lead Owner','error')
         })
-
     }
 
-    showToast(title, message, variant){
-        this.dispatchEvent(new ShowToastEvent({
-            title : this.title,
-            message : message,
-            variant : variant 
-        }));
+    showToast(title,message,variant){
+        this.dispatchEvent(
+            new ShowToastEvent({
+                title : title,
+                message : message,
+                variant : variant
+            })
+        );
     }
 
 }
