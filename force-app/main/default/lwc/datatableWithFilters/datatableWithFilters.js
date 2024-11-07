44d218cc3880bc6bb1933900071e86054606c534
Venkata Sai Charan Bhupathi <venkatasaicharanbhupathi.com>
2024-11-07 19:02:47 +0530

    Display list of cases related accounts contact and add filters

---
+++ force-app/main/default/lwc/datatableWithFilters/datatableWithFilters.js
@@ -0,0 +1,72 @@
+import { api, LightningElement, wire } from 'lwc';
+import getCaseList from '@salesforce/apex/datatableWithFiltersController.getCaseList';
+
+export default class DatatableWithFilters extends LightningElement {
+
+    @api recordId;
+    contacts = [];
+    cases = [];
+
+    columns = [
+        {label : 'Case Number', fieldName : 'caseUrl', type : 'url', typeAttributes : { label : {fieldName : 'CaseNumber'}}},
+        {label : 'Case Status', fieldName : 'Status'},
+        {label : 'Created Date', fieldName : 'CreatedDate'},
+        {label : 'Last Modified Date', fieldName : 'LastModifiedDate'}
+    ];
+
+    statusValue = '';
+    dateValue = '';
+    contactsValue = '';
+
+    get statusOptions(){
+        return [
+            {label : 'All', value : ''},
+            {label : 'New', value : 'New'},
+            {label : 'Working', value : 'Working'},
+            {label : 'Escalated', value : 'Escalated'},
+        ]
+    }
+
+    get dateOptions(){
+        return [
+            {label : 'All', value : ''},
+            {label : 'Today', value : 'today'},
+            {label : 'Last 30 Days', value : 'last30days'}
+        ]
+    }
+
+    get contactOptions(){
+        return [
+            {label : 'All', value : ''},
+            ...this.contacts.map(contact=>({
+                label : contact.Name,
+                value : contact.Id
+            }))
+        ];
+    }
+
+    handleStatus(event){
+        this.statusValue = event.target.value;
+    }
+
+    handleDate(event){
+        this.dateValue = event.target.value;
+    }
+    
+    handleContacts(event){
+        this.contactsValue = event.target.value;
+    }
+
+    @wire(getCaseList,{accId : '$recordId', status : '$statusValue', date : '$dateValue', contact : '$contactsValue'})
+    wiredData({data,error}){
+        if(data){
+            this.contacts = data.contacts;
+            this.cases = data.cases.map(caselist=>({
+                ...caselist,
+                caseUrl : `${caselist.Id}`
+            }));
+        }else{
+            console.log('Error', JSON.stringify(error));
+        }
+    }
+}
