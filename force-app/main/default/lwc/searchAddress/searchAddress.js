1a388dffe456c36657b948c790c19e094a5921b7
Venkata Sai Charan Bhupathi <venkatasaicharanbhupathi.com>
2024-11-29 17:11:24 +0530

    .

--- force-app/main/default/lwc/searchAddress/searchAddress.js
+++ force-app/main/default/lwc/searchAddress/searchAddress.js
@@ -6,6 +6,11 @@ export default class SearchAddress extends LightningElement {
     searchKey = '';
     suggestions = [];
 
+    address1 ='';
+    address2 = '';
+    postCode = '';
+    city = '';
+
     handleSearch(e){
         this.searchKey = e.target.value;
             this.searchResults();
@@ -16,12 +21,32 @@ export default class SearchAddress extends LightningElement {
         // console.log('Search results: ', val);
         fetchAddress({query : this.searchKey})
         .then(result => {
-            console.log('result:', result);
+            // console.log('result:', result);
             this.suggestions = result;
-            console.log('suggestions:', this.suggestions);
+            // console.log('suggestions:', this.suggestions);
         })
         .catch(error=>{
             console.error('Error: ', error);
         })
+    }
+
+    handleSelect(event){
+
+        console.log('event ===>' ,event.target.dataset.value);
+        
+        const fullAddress = event.target.dataset.value;
+
+        const splitAddress = fullAddress.split(',')
+
+        this.address1 = splitAddress[0];
+        this.address2 = splitAddress[1];
+
+        const postndcity = splitAddress[splitAddress.length - 1].split(' ');
+        
+        this.postCode = postndcity[1];
+
+        this.city = postndcity[2];
+
+
     }
 }
